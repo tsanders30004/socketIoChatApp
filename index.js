1e12bba82d4a284747b884b0cda7d8d755fee526
Tim Sanders <tsanders30004@gmail.com>
2016-06-28 13:56:29 -0400

    28 Jun 2016
    Nickname support now functions.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http').Server(app);      /* creates a server */
+var io = require('socket.io')(http);         
 
 app.get('/', function(req, res){
      res.sendfile('index.html');
@@ -13,25 +13,10 @@ io.on('connection', function(socket){
      });
 });
 
-
-
 /* show a message whenever a new user logs on */
-io.on('connection', function(client){
-     client.emit('chat message', 'new user logged on');
+io.on('connection', function(socket){
+     socket.broadcast.emit('chat message', 'new user logged on xxx');
 });
-
-// io.on('connection', function(msg){
-//      io.emit('chat message', msg);
-// });
-
-/* need to boradcast a message when someone logs on or off */
-
-     // socket.on('disconnect', function(){
-     //      console.log('user disconnected');
-     // });
-
-
-
 
 http.listen(3000, function(){
      console.log('listening on *:3000');
